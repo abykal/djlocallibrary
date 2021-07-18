ad625bf97407fa513a15c78396d05bd02e671ce0
Aby Kal <abyabrkal@gmail.com>
2021-07-18 15:14:13 +0400

    Admin updated with new views and advanced view and filtering options

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,27 +1,53 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
+"""Minimal registration of Models.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
+"""
 
 # Register your models here.
-#admin.site.register(Book)
 admin.site.register(Genre)
-#admin.site.register(Author)
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-admin.site.register(Author, AuthorAdmin)
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
-    def display_genre(self):
-        """Create a string for the Genre. This is required to display genre in Admin."""
-        return ', '.join(genre.name for genre in self.genre.all()[:3])
 
-    display_genre.short_description = 'Genre'
+
 
 # Register the Admin classes for BookInstance using the decorator
-@admin.register(BookInstance)
+@admin.register(BookInstance)   #does the same as admin.site.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
-#admin.site.register(BookInstance)
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
