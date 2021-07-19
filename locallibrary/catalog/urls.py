9462e9d36d9976e8a67da4c5a4ce9eb82495c998
Aby Kal <abyabrkal@gmail.com>
2021-07-19 15:54:59 +0400

    Class based views

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/<int:pk>', views.BookDetailListView.as_view(), name='book-detail'),
 ]
