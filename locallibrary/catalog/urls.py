cd05035dc38d6aaa20544a3916f8243f2c1a890a
Aby Kal <abyabrkal@gmail.com>
2021-07-20 13:35:16 +0400

    Bootstrap styling across with cards and new layouts

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('books/<int:pk>', views.BookDetailListView.as_view(), name='book-detail'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -30,8 +30,23 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 10
     # Other options that can be added to customize the default options.
     # -----------------------------------------------
     # context_object_name = 'my_book_list'  # your own name for the list as a template variable
     # queryset = Book.objects.filter(title__icontains='war')[:5]  # Get 5 books containing the title war
     # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
