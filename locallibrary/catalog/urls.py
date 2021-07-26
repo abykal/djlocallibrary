6ffcc686317219b3477769d15a5c40dbae453b68
Aby Kal <abyabrkal@gmail.com>
2021-07-26 16:24:29 +0400

    User permissions completed.

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('author/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
